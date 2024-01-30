0959a07e6080f01632c51a63162c6b0974a1cca9
borgesigor <igorblima01@gmail.com>
2024-01-30 18:01:55 -0300

    video player

--- src/Components/VideoPlayer/VideoPlayer.tsx
+++ src/Components/VideoPlayer/VideoPlayer.tsx
@@ -1,66 +1,102 @@
 import dashjs from "dashjs";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 const url = 
-  "https://rdmedia.bbc.co.uk/testcard/simulcast/manifests/avc-full.mpd"
-  // "https://dash.akamaized.net/dash264/TestCases/1a/sony/SNE_DASH_SD_CASE1A_REVISED.mpd"
+  // "https://rdmedia.bbc.co.uk/testcard/simulcast/manifests/avc-full.mpd"
+  "https://dash.akamaized.net/dash264/TestCases/1a/sony/SNE_DASH_SD_CASE1A_REVISED.mpd"
 
 export function VideoPlayerComponent() {
-  const [durationText, setDurationText] = useState('');
-  const [actualTimeText, setActualTimeText] = useState('');
-
-  const videoRef = useRef(null);
+  const videoRef: any = useRef(null);
   const player: any = useRef(null);
-  const subtitle = useRef(null);
+  const subtitle: any = useRef(null);
   const progressBar: any = useRef(null);
+  const isSeeking: any = useRef(false);
 
-  let isSeeking = false;
+  let progressInterval: any;
 
-  function streamingDuration(){
-    return player.current.getDVRWindowSize() - player.current.getTargetLiveDelay()
+  // Update - atualizar tudo
+  function update(){
+    timecode()
+    updateProgressBarDuration()
+    updateProgressBar()
+    changePercent()
   }
 
-  function convertToTimeCode(time: number){
-    return player.current.convertToTimeCode(time)
-  }
+  // Utils - utilidades
+  const streamingDuration = useCallback(() => {
+    return player.current.getDVRWindowSize() - player.current.getTargetLiveDelay();
+  }, []);
 
-  function updateProgressBar(){
-    if(isSeeking === false){
-      progressBar.current.value = player.current.time()
+  const convertToTimeCode = useCallback((time: number) => {
+    return player.current.convertToTimeCode(time);
+  }, []);
+
+  const [ percentOfVideo, setPercentOfVideo ] = useState(0)
+
+  const changePercent = () => {
+    if(player.current){
+      setPercentOfVideo((progressBar.current.value / progressBar.current.max) * 100)
     }
-
-    setActualTimeText(convertToTimeCode(streamingDuration() - player.current.time()))
   }
 
-  function updateDuration(){
-    if(player.current.isDynamic()){
-      progressBar.current.max = streamingDuration()
-    }else{
-      progressBar.current.max = player.current.duration()
+  // IsLive - verificar se é uma transmissão ao vivo
+  const isLive = () => {
+    if(player.current && player.current.isDynamic()){
+      return true
     }
-
-    setDurationText(convertToTimeCode(streamingDuration()))
+    return false
   }
 
-  function seeking(){
-    isSeeking = true;
-    player.current.pause()
-  }
-
-  function seekingEnd(){
-
-    if(!(player.current && progressBar.current)){
-      throw new Error('controles não criados.')
+  // Update - atualizaçao do progress bar
+  const updateProgressBar = useCallback(() => {
+    if (isSeeking.current === false) {
+      progressBar.current.value = player.current.time();
     }
+  }, [convertToTimeCode, streamingDuration]);
 
-    player.current.seek(parseFloat(progressBar.current.value))
-    player.current.play()
+  const updateProgressBarDuration = useCallback(() => {
+    if (isLive()) {
+      progressBar.current.max = streamingDuration();
+    } else {
+      progressBar.current.max = player.current.duration();
+    }
+  }, [convertToTimeCode, streamingDuration]);
 
-    isSeeking = false;
+  // Screen Data - alterações nos dados mostrados na tela
 
+  const [durationTimecode, setDurationTimecode] = useState('');
+  const [actualTimecode, setActualTimecode] = useState('');
+
+  const timecode = useCallback(() => {
+    setActualTimecode(convertToTimeCode(player.current.time()));
+    setDurationTimecode(convertToTimeCode(player.current.duration()));
+  }, [convertToTimeCode]);
+
+  // Seeking - ao selecionar o progress bar
+  const seeking = useCallback(() => {
+    isSeeking.current = true;
+    player.current.pause();
+    clearInterval(progressInterval);
+  }, [progressInterval]);
+
+  const seekingEnd = useCallback(() => {
+    player.current.seek(parseFloat(progressBar.current.value));
+    player.current.play();
+
+    isSeeking.current = false;
+    progressInterval = setInterval(() => {
+      update()
+    }, 100);
+  }, [updateProgressBarDuration, updateProgressBar]);
+
+  const seekToLiveEdge = () => {
+    if(isLive()){
+      player.current.seekToOriginalLive()
+    }
   }
 
+  //
   useEffect(() => {
 
     if (videoRef.current) {
@@ -68,49 +104,53 @@ export function VideoPlayerComponent() {
 
       player.current = dashjs.MediaPlayer().create();
 
-      player.current.initialize(video, url, true);
+      player.current.initialize(video, url, false);
       player.current.attachView(video);
       player.current.attachTTMLRenderingDiv(subtitle.current);
 
-      
-      setInterval(()=>{
-        updateDuration()
-        updateProgressBar()
-      }, 100)
-
+      progressInterval = setInterval(() => {
+        update()
+      }, 100);
     }
-    
+
     return () => {
       if (player.current) {
         player.current.destroy();
         player.current = null;
       }
+
+      clearInterval(progressInterval);
     };
 
-  }, [player.current]);
+  }, [updateProgressBarDuration, updateProgressBar]);
 
   return (
     <>
       <div style={{ position: 'relative' }}>
         <div ref={subtitle}></div>
-        <video ref={videoRef} autoPlay style={{ width: "100%" }} controls></video>
+        <video ref={videoRef} style={{ width: "100%" }}></video>
         <ControlBarContainer>
           <ControlBarUpper>
-            <input 
-              ref={progressBar}  
-              type="range"
-              onMouseDown={seeking}
-              onMouseUp={seekingEnd}
-              style={{ width: '100%' }}
-              step={0.1}
-            />
+            <ProgressBar>
+              <input ref={progressBar} type="range" onMouseDown={seeking} onTouchStart={seeking} onMouseUp={seekingEnd} onTouchEnd={seekingEnd} onChange={changePercent} style={{ width: '100%' }} step={1} />
+              <div className="new-input">
+                <div className="track tracks"></div>
+                <div className="past tracks" style={{ width: `${percentOfVideo}%` }} ></div>
+              </div>
+            </ProgressBar>
           </ControlBarUpper>
           <ControlBarDown>
-            <Infos>
-              <span>{actualTimeText}</span>
-              <span>/</span>
-              <span>{durationText}</span>
-            </Infos>
+            <button>Play</button>
+            {
+              isLive() ?
+                <button onClick={seekToLiveEdge} >Live</button>
+              :
+                <Timecode>
+                  <span className="actual">{actualTimecode}</span>
+                  <span className="separator">/</span>
+                  <span className="duration">{durationTimecode}</span>
+                </Timecode>
+            }
           </ControlBarDown>
         </ControlBarContainer>
       </div>
@@ -118,20 +158,108 @@ export function VideoPlayerComponent() {
   );
 }
 
-const Infos = styled.div`
-
-`
-
 const ControlBarContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-`
-
-const ControlBarUpper = styled.div`
-
+  position: absolute;
+  bottom: 0;
+  box-sizing: border-box;
+  padding: 1rem 1rem;
+  color: white;
+  background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(24,24,24,1) 100%);
 `
 
 const ControlBarDown = styled.div`
+  padding: 0.5rem 0rem;
+`
+
+const ProgressBar = styled.div`
+  position: relative;
+
+  .new-input{
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    z-index: 1;
+  }
+
+  .tracks{
+    height: 2px;
+    position: absolute;
+  }
+  
+  .track{
+    width: 100%;
+    background: #ffffff5c;
+  }
+
+  .past{
+    background: white;
+  }
+
+  input{
+    position: relative;
+    z-index: 2;
+    appearance: none;
+    -webkit-appearance: none;
+    margin: 0;
+    width: 100%;
+    background: transparent;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    height: 1.5rem;
+    /* background: red; */
+ 
+    &::-webkit-slider-thumb{
+      -webkit-appearance: none;
+      appearance: none;
+      width: 0.6rem;
+      height: 0.6rem;
+      background: white;
+      border-radius: 50%;
+      position: relative;
+      top: 0.5px;
+      transition: 100ms;
+    }
+  }
+
+  &:hover{
+    input::-webkit-slider-thumb{
+      width: 1rem;
+      height: 1rem;
+    }
+
+    .tracks{
+      height: 4px;
+    }
+  }
+
+  /* &:active{
+    input::-webkit-slider-thumb{
+      width: 1.2rem;
+      height: 1.2rem;
+    }
+  } */
+`
+
+const Timecode = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  font-family: ${props => props.theme.secondaryFont};
+  align-items: center;
+  font-size: 0.9rem;
+
+  .separator{
+    opacity: 0.3;
+  }
+`
+
+const ControlBarUpper = styled.div`
 
 `
