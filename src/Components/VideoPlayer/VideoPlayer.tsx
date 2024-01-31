903000d496153c4fdd0acce5525b0759853dfd6f
borgesigor <igorblima01@gmail.com>
2024-01-31 17:32:03 -0300

    video player changes

--- src/Components/VideoPlayer/VideoPlayer.tsx
+++ src/Components/VideoPlayer/VideoPlayer.tsx
@@ -5,162 +5,160 @@ import { isMobile } from "../../Utils/GetDeviceType";
 
 const url = 
   // "https://rdmedia.bbc.co.uk/testcard/simulcast/manifests/avc-full.mpd"
-  "https://dash.akamaized.net/dash264/TestCases/1a/sony/SNE_DASH_SD_CASE1A_REVISED.mpd"
+  // "https://dash.akamaized.net/dash264/TestCases/1a/sony/SNE_DASH_SD_CASE1A_REVISED.mpd"
+  "http://localhost/tmp_dash/stream/index.mpd"
   // "https://rdmedia.bbc.co.uk/testcard/lowlatency/manifests/ll-avc-full.mpd"
   // "https://d24rwxnt7vw9qb.cloudfront.net/v1/dash/e6d234965645b411ad572802b6c9d5a10799c9c1/All_Reference_Streams/6ba06d17f65b4e1cbd1238eaa05c02c1/index.mpd"
 
 export function VideoPlayerComponent() {
-  const videoRef: any = useRef(null);
-  const player: any = useRef(null);
-  const subtitle: any = useRef(null);
-  const progressBar: any = useRef(null);
-  const volumeSlider: any = useRef(null);
-  const isSeeking: any = useRef(false);
+  const videoContainer: any = useRef(null)
+  const videoRef: any = useRef(null)
+  const player: any = useRef(null)
+  const subtitle: any = useRef(null)
+  const sliderProgress: any = useRef(null)
+  const volumeSlider: any = useRef(null)
+  const isSeeking: any = useRef(false)
+  const userPaused: any = useRef(false)
+  const isLive: any = useRef(false)
 
-  const [isPaused, setIsPaused] = useState(false);
-  const [userPaused, setUserPaused] = useState(false);
+  const [paused, setPaused] = useState(true)
+  const [duration, setDuration] = useState(0)
+  const [actualTime, setActualTime] = useState(0)
+  const [isLoading, setIsLoading] = useState(false)
+  const [videoPercentage, setVideoPercentage] = useState(0)
+  const [iconVolume, setIconVolume] = useState(0)
 
-  let progressInterval: any;
-
-  // Update - atualizar tudo
-  function update(){
-    timecode()
-    updateProgressBarDuration()
-    updateProgressBar()
-    changePercent()
-    sliderVolumeSync()
-    setIsPaused(player.current.isPaused())
+  function convertToTimeCode(value: number){
+    return player.current.convertToTimeCode(value)
   }
 
-  const manualPause = () => {
-    setUserPaused(true)
-    player.current.pause()
+  function onPlay(){
+    setPaused(false)
   }
 
-  const manualPlay = () => {
-    setUserPaused(false)
-    player.current.play()
+  function onPause(){
+    setPaused(true)
   }
 
-  // Toggle
-  const togglePlayPause = () => {
-    isPaused ? manualPlay() : manualPause()
+  function onPlaybackWaiting(){
+    setIsLoading(true)
   }
 
-  // Utils - utilidades
-  const streamingDuration = useCallback(() => {
-    return player.current.getDVRWindowSize() - player.current.getTargetLiveDelay();
-  }, []);
+  function canPlay(){
+    player.current.isDynamic() ? isLive.current = true : isLive.current = false
 
-  const convertToTimeCode = useCallback((time: number) => {
-    return player.current.convertToTimeCode(time);
-  }, []);
+    setIsLoading(false)
+    setApplicationVolume()
+  }
 
-  const [ percentOfVideo, setPercentOfVideo ] = useState(0)
-
-  const changePercent = () => {
-    if(player.current){
-      setPercentOfVideo((progressBar.current.value / progressBar.current.max) * 100)
+  function toggleFullscreen(){
+    if(document.fullscreenElement){
+      document.exitFullscreen()
+    }else{
+      videoContainer.current.requestFullscreen()
     }
   }
 
-  // IsLive - verificar se é uma transmissão ao vivo
-  const isLive = () => {
-    if(player.current && player.current.isDynamic()){
-      return true
-    }
-    return false
+  function getPlayerVolume(){
+    return player.current.isMuted() ? 0 : player.current.getVolume()
   }
 
-  // Update - atualizaçao do progress bar
-  const updateProgressBar = useCallback(() => {
-    if (isSeeking.current === false) {
-      progressBar.current.value = player.current.time();
-    }
-  }, [convertToTimeCode, streamingDuration]);
+  function changePlayerVolume(){
+    player.current.setVolume(parseFloat(volumeSlider.current.value))
+    volumeSlider.current.value > 0 ? player.current.setMute(false) : player.current.setMute(true)
+    setIconVolume(getPlayerVolume())
+  }
 
-  const updateProgressBarDuration = useCallback(() => {
-    if (isLive()) {
-      progressBar.current.max = streamingDuration();
+  function setApplicationVolume(){
+    volumeSlider.current.value = getPlayerVolume()
+    setIconVolume(getPlayerVolume())
+  }
+
+  function togglePlayPause(){
+    if (player.current.isPaused()){
+      player.current.play()
+      userPaused.current = false
     } else {
-      progressBar.current.max = player.current.duration();
-    }
-  }, [convertToTimeCode, streamingDuration]);
-
-  // Screen Data - alterações nos dados mostrados na tela
-
-  const [durationTimecode, setDurationTimecode] = useState('');
-  const [actualTimecode, setActualTimecode] = useState('');
-
-  const timecode = useCallback(() => {
-    setActualTimecode(convertToTimeCode(player.current.time()));
-    setDurationTimecode(convertToTimeCode(player.current.duration()));
-  }, [convertToTimeCode]);
-
-  // Seeking - ao selecionar o progress bar
-  const seeking = useCallback(() => {
-    isSeeking.current = true;
-
-    player.current.pause();
-    
-    clearInterval(progressInterval);
-  }, [progressInterval]);
-
-  const seekingEnd = useCallback(() => {
-    player.current.seek(parseFloat(progressBar.current.value));
-    
-    if(!userPaused){
-      player.current.play();
-    }
-
-    isSeeking.current = false;
-
-    progressInterval = setInterval(() => {
-      update()
-    }, 100);
-  }, [updateProgressBarDuration, updateProgressBar, userPaused]);
-
-  const seekToLiveEdge = () => {
-    if(isLive()){
-      player.current.seekToOriginalLive()
+      player.current.pause()
+      userPaused.current = true
     }
   }
 
-  // Volume 
-
-  const [userVolume, setUserVolume] = useState(0);
-
-  const toggleVolume = useCallback(()=>{
-    // userVolume ? player.current.setVolume(userVolume) : player.current.setVolume(100)
-    console.log(player.current.isMuted())
-  }, [])
-
-  const setMuted = (muted: boolean) => {
-    if(muted){
-      player.current.setMuted(true)
+  function mediaDuration(){
+    if(!isLive.current){
+      return player.current.duration()
     }
 
-    player.current.setMuted(false)
+    return player.current.getDVRWindowSize() - player.current.getTargetLiveDelay()
   }
 
-  const setVolume = useCallback(() => {
-    const volume = volumeSlider.current.value / 100
+  function syncToTheEdge(){
+    player.current.seekToOriginalLive();
 
-    player.current.setVolume(volume)
+    player.current.off(dashjs.MediaPlayer.events.CAN_PLAY, syncToTheEdge);
+  }
 
-    if(volume == 0){
-      
+  function seekToLiveEdge(){
+    player.current.seekToOriginalLive();
+
+    player.current.on(dashjs.MediaPlayer.events.CAN_PLAY, syncToTheEdge);
+  }
+
+  function sliderProgressUpdate(){
+    if(!isSeeking.current){
+      sliderProgress.current.value = player.current.time()
+      sliderProgress.current.max = mediaDuration()
+    }
+  }
+
+  function timeCodeUpdate(){
+    setDuration(convertToTimeCode(sliderProgress.current.max))
+    setActualTime(convertToTimeCode(sliderProgress.current.value))
+  }
+
+  function sliderTrackUpdate(){
+    setVideoPercentage((sliderProgress.current.value / sliderProgress.current.max) * 100)
+  }
+
+  function sliderSeeking(){
+    timeCodeUpdate()
+    sliderTrackUpdate()
+  }
+
+  function sliderSeekStart(){
+    player.current.pause()
+    isSeeking.current = true
+  }
+
+  function sliderSeekEnd(){
+    player.current.seek(parseFloat(sliderProgress.current.value))
+    
+    if(!userPaused.current){
+      player.current.play()
     }
 
-  }, []);
+    isSeeking.current = false
+  }
 
-  const sliderVolumeSync = useCallback(() => {
-    if(!isMobile()){
-      volumeSlider.current.value = player.current.getVolume() * 100
-    }
-  }, []);
+  // function nowTime(){
+  //   let nowTime = 'não sei: ' + convertToTimeCode(player.current.getDVRWindowSize()) + ' duração: ' + convertToTimeCode(player.current.getDVRSeekOffset(0))
+  //   return nowTime
+  // }
 
-  //
+  function timeUpdate(){
+    sliderProgressUpdate()
+    timeCodeUpdate()
+    sliderTrackUpdate()
+
+    // console.info(nowTime())
+    // console.log(sliderProgress.current.value)
+  }
+
+  function firstLoad(){
+    sliderProgress.current.value = 0;
+    volumeSlider.current.value = 0;
+  }
+
   useEffect(() => {
 
     if (videoRef.current) {
@@ -168,13 +166,23 @@ export function VideoPlayerComponent() {
 
       player.current = dashjs.MediaPlayer().create();
 
-      player.current.initialize(video, url, false);
+      player.current.initialize(video, url, true);
       player.current.attachView(video);
       player.current.attachTTMLRenderingDiv(subtitle.current);
 
-      progressInterval = setInterval(() => {
-        update()
-      }, 100);
+      firstLoad()
+
+      player.current.on(dashjs.MediaPlayer.events.PLAYBACK_PLAYING, onPlay);
+      player.current.on(dashjs.MediaPlayer.events.PLAYBACK_PAUSED, onPause);
+      player.current.on(dashjs.MediaPlayer.events.PLAYBACK_WAITING, onPlaybackWaiting);
+      player.current.on(dashjs.MediaPlayer.events.CAN_PLAY, canPlay);
+      player.current.on(dashjs.MediaPlayer.events.PLAYBACK_TIME_UPDATED, timeUpdate);
+      // player.current.on(dashjs.MediaPlayer.events.TEXT_TRACKS_ADDED, subtitlesUpdate)
+
+      sliderProgress.current.addEventListener('mousedown', sliderSeekStart);
+      sliderProgress.current.addEventListener('mouseup', sliderSeekEnd);
+      // sliderProgress.current.addEventListener('change', sliderSeeking);
+
     }
 
     return () => {
@@ -182,24 +190,48 @@ export function VideoPlayerComponent() {
         player.current.destroy();
         player.current = null;
       }
-
-      clearInterval(progressInterval);
     };
 
-  }, [updateProgressBarDuration, updateProgressBar]);
+  }, [player.current]);
 
   return (
     <>
-      <div style={{ position: 'relative' }}>
+      <div style={{ position: 'relative', aspectRatio: '16 / 9', background: 'black', overflow: 'hidden' }} ref={videoContainer} >
         <div ref={subtitle}></div>
-        <video ref={videoRef} style={{ width: "100%" }}></video>
+        <video ref={videoRef} style={{ width: "100%" }} ></video>
+        <SpinnerContainer>
+          {
+            isLoading &&
+              <Spinner>
+                  <svg viewBox="25 25 50 50">
+                    <circle r="20" cy="50" cx="50"></circle>
+                  </svg>
+              </Spinner>
+          }
+        </SpinnerContainer>
+        <DataContainer>
+          <Card>
+            <span className="tag" style={{ fontSize: '0.9em' }} >Acervo</span>
+            <div className="texts">
+              <span style={{ fontSize: '1em' }}>Testemunho</span>
+              <span style={{ fontWeight: 400, fontSize: '0.9em', opacity: 0.6 }} >João</span>
+            </div>
+          </Card>
+        </DataContainer>
+        <ScreenClickContainer $paused={userPaused.current} onClick={togglePlayPause}>
+          <button>
+            <span style={{ fontSize: '2rem' }} className="material-symbols-rounded">
+              { userPaused.current ? 'play_arrow' : 'pause' }
+            </span>
+          </button>
+        </ScreenClickContainer>
         <ControlBarContainer>
           <ControlBarUpper>
             <ProgressBar>
-              <input ref={progressBar} type="range" onMouseDown={seeking} onTouchStart={seeking} onMouseUp={seekingEnd} onTouchEnd={seekingEnd} onChange={changePercent} style={{ width: '100%' }} step={1} />
+              <input ref={sliderProgress} type="range" style={{ width: '100%' }} step={1} onChange={sliderSeeking} />
               <div className="new-input">
                 <div className="track tracks"></div>
-                <div className="past tracks" style={{ width: `${percentOfVideo}%` }} ></div>
+                <div className="past tracks" style={{ width: `${videoPercentage}%` }} ></div>
               </div>
             </ProgressBar>
           </ControlBarUpper>
@@ -209,18 +241,18 @@ export function VideoPlayerComponent() {
                 !isMobile() &&
                   <button onClick={togglePlayPause} >
                     <span style={{ fontSize: '2rem' }} className="material-symbols-rounded">
-                      { isPaused ? 'play_arrow' : 'pause' }
+                      { paused ? 'play_arrow' : 'pause' }
                     </span>
                   </button>
               }
               {
-                isLive() ?
-                  <button className="live-button" onClick={seekToLiveEdge} >Ao Vivo</button>
+                isLive.current ?
+                  <LiveButton $isLiveEdge={videoPercentage > 99} onClick={seekToLiveEdge} >Ao Vivo</LiveButton>
                 :
                   <Timecode>
-                    <span className="actual">{actualTimecode}</span>
+                    <span className="actual">{actualTime ? actualTime : '0:00'}</span>
                     <span className="separator">/</span>
-                    <span className="duration">{durationTimecode}</span>
+                    <span className="duration">{duration ? duration : '0:00'}</span>
                   </Timecode>
               }
             </ControlBarDownLeft>
@@ -228,15 +260,20 @@ export function VideoPlayerComponent() {
               {
                 !isMobile() &&
                   <Volume>
-                    <button onClick={toggleVolume} >
+                    <button >
                       <span className="material-symbols-rounded">
-                        volume_up
+                        { 
+                          iconVolume > 0 && 'volume_up'
+                        }
+                        {
+                          iconVolume == 0 && 'volume_off'
+                        }
                       </span>
                     </button>
                     <div className="volume-dropdown">
 
                       <ProgressBar>
-                        <input style={{ width: '100%' }} step={1} max={100} type="range" ref={volumeSlider} onChange={setVolume} />
+                        <input ref={volumeSlider} step={0.01} max={1} type="range" onChange={changePlayerVolume} />
                         <div className="new-input">
                           <div className="track tracks"></div>
                           <div className="past tracks"></div>
@@ -246,7 +283,7 @@ export function VideoPlayerComponent() {
                     </div>
                   </Volume>
               }
-              <button>
+              <button onClick={toggleFullscreen}>
                 <span className="material-symbols-rounded"> 
                   fullscreen 
                 </span>
@@ -258,6 +295,159 @@ export function VideoPlayerComponent() {
     </>
   );
 }
+
+const DataContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 1rem;
+`
+
+const Card = styled.div`
+  background: white;
+  display: flex;
+  align-items: center;
+  padding: 1.5rem 2rem;
+  box-sizing: border-box;
+  font-weight: 600;
+  font-size: 1rem;
+  gap: 1.5rem;
+  color: #000;
+  overflow: hidden;
+  opacity: 0;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.20);
+  border-radius: 0.2rem;
+  animation: move 22s ease;
+
+  .texts{
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .tag{
+    border: 1px solid;
+    padding: 0.5em 1em;
+    border-radius: 0.2em;
+    border-color: #666;
+    animation: move 22s ease;
+  }
+
+  @keyframes move {
+    0%{
+      transform: translateX(-200%);
+      opacity: 0;
+      width: -0%;
+    }
+    5%{
+      transform: translateX(0%);
+      width: 100%;
+      opacity: 1;
+    }
+    95%{
+      transform: translateX(0%);
+      width: 100%;
+      opacity: 1;
+    }
+    100%{
+      transform: translateX(-200%);
+      opacity: 0;
+      width: -0%;
+    }
+  }
+`
+
+const SpinnerContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+`
+
+const ScreenClickContainer = styled.div<{ $paused: boolean }>`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  button{
+    border-radius: 100px;
+    border: none;
+    background: #ffffff;
+    color: #141414;
+    height: 3rem;
+    width: 3rem;
+    /* opacity: ${props => props.$paused ? 'fadeInOut' : '0'}; */
+    opacity: 0;
+    ${props => props.$paused && 'animation: fadeInOut .5s ease-in-out;' }
+  }
+
+  @keyframes fadeInOut {
+    0% {
+      opacity: 0;
+      transform: scale(1);
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+      transform: scale(1.1);
+    }
+  }
+`
+
+const Spinner = styled.div`
+  background: rgba(0,0,0,0.4);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    width: 5em;
+    transform-origin: center;
+    animation: rotate4 2s linear infinite;
+  }
+
+  circle {
+    fill: none;
+    stroke: rgba(255, 255, 255, 1);
+    stroke-width: 4;
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 2;
+    stroke-linecap: round;
+    animation: dash4 1.5s ease-in-out infinite;
+  }
+
+  @keyframes rotate4 {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes dash4 {
+    0% {
+      stroke-dasharray: 1, 200;
+      stroke-dashoffset: 0;
+    }
+
+    50% {
+      stroke-dasharray: 90, 200;
+      stroke-dashoffset: -35px;
+    }
+
+    100% {
+      stroke-dashoffset: -125px;
+    }
+  }
+`
 
 const Volume = styled.div`
   display: flex;
@@ -297,21 +487,21 @@ const ControlBarContainer = styled.div`
       position: relative;
     }
   }
+`
 
-  .live-button{
-    padding: 0;
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
+const LiveButton = styled.button<{ $isLiveEdge: boolean }>`
+  padding: 0;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 
-    &::before{
-      content: '';
-      width: 8px;
-      height: 8px;
-      background: red;
-      display: block;
-      border-radius: 100px;
-    }
+  &::before{
+    content: '';
+    width: 8px;
+    height: 8px;
+    background: ${props => props.$isLiveEdge ? 'red' : 'grey'};
+    display: block;
+    border-radius: 100px;
   }
 `
 
@@ -354,10 +544,12 @@ const ProgressBar = styled.div`
   .track{
     width: 100%;
     background: #ffffff5c;
+    /* background: #43a4ff8b; */
   }
 
   .past{
     background: white;
+    /* background: #43a4ff; */
   }
 
   input{
@@ -380,6 +572,7 @@ const ProgressBar = styled.div`
       width: 0.6rem;
       height: 0.6rem;
       background: white;
+      /* background: #43a4ff; */
       border-radius: 50%;
       position: relative;
       top: 1px;
